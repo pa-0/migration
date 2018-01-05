551f6dde821b8c9a870c9e56756ec123f6cc3eb1
Cyrus Harrison <cyrush@llnl.gov>
2018-01-05 09:10:38 -0800

    refactor

---
+++ git_graft.py
@@ -0,0 +1,51 @@
+#######################################
+# git_graft.py
+#######################################
+
+import glob
+import shutil
+
+from os.path import join as pjoin
+
+from general_utils import *
+from svn_utils import *
+from git_utils import *
+
+def git_graft():
+    svn_check_rc_git_svn_repos()
+    rcs = svn_ls_rc_branches()
+    git_setup_new_repo()
+    git_connect_git_svn_remotes()
+    git_setup_develop()
+    # for each rc, create rc branch
+    for rc in rcs:
+        if os.path.isdir( git_svn_rc_checkout_dir(rc)):
+            git_create_rc_branch(rc)
+    # for each release, create squashed commit to master
+    for rc in rcs:
+      for release in svn_release_tags_for_rc(rc):
+           git_create_branch_for_tag_release(release)
+           git_merge_release_to_master_and_tag(release)
+
+git_graft()
+
+# create each release on master
+# for each tag, prep a branch off of the rc that will provide a source for us
+# to squash merge to #  master  to do this, we need the commit ranges for the tags
+
+#git_create_rc_branch("2.0RC")
+#git_create_branch_for_tag_release("2.0.0")
+#git_merge_release_to_master_and_tag("2.0.0")
+#git_create_branch_for_tag_release("2.0.2")
+#git_merge_release_to_master_and_tag("2.0.2")
+
+#git_create_rc_branch("2.1RC")
+
+#git_create_branch_for_tag_release("2.1.1")
+#git_merge_release_to_master_and_tag("2.1.1")
+#git_create_branch_for_tag_release("2.1.1")
+#git_merge_release_to_master_and_tag("2.1.1")
+
+
+
+
--- git_utils.py
+++ git_utils.py
@@ -1,5 +1,5 @@
 #######################################
-# git_graft.py
+# git_utils.py
 #######################################
 
 import glob
@@ -9,7 +9,6 @@ from os.path import join as pjoin
 
 from general_utils import *
 from svn_utils import *
-
 
 
 def git_add_remote(name,path):
@@ -143,7 +142,6 @@ def git_create_rc_branch(rc):
             # checkout 'thiers' to resolve final conflicts
             git_conflicts_checkout_and_add_theirs()
             sexe('git commit -m "%s"' % (merge_msg))
-
   
 def git_create_branch_for_tag_release(tag):
     rc = svn_tag_to_rc(tag)
@@ -168,46 +166,6 @@ def git_merge_release_to_master_and_tag(tag):
         # TODO: add --author="John Doe <john@doe.com>"         
         sexe('git commit -m "%s release"' % tag)
         sexe('git tag -a v%s -m "tag r%s"' % (tag,tag))
-
-
-def git_graft():
-    svn_check_rc_git_svn_repos()
-    rcs = svn_ls_rc_branches()
-    git_setup_new_repo()
-    git_connect_git_svn_remotes()
-    git_setup_develop()
-    # for each rc, create rc branch
-    for rc in rcs:
-        if os.path.isdir( git_svn_rc_checkout_dir(rc)):
-            git_create_rc_branch(rc)
-    # for each release, create squashed commit to master
-    for rc in rcs:
-      for release in svn_release_tags_for_rc(rc):
-           git_create_branch_for_tag_release(release)
-           git_merge_release_to_master_and_tag(release)
-
-#git_graft()
-
-with cchdir(git_repo_dir()):
-    git_ls_conflicts()
-
-
-# create each release on master
-# for each tag, prep a branch off of the rc that will provide a source for us
-# to squash merge to #  master  to do this, we need the commit ranges for the tags
-
-#git_create_rc_branch("2.0RC")
-#git_create_branch_for_tag_release("2.0.0")
-#git_merge_release_to_master_and_tag("2.0.0")
-#git_create_branch_for_tag_release("2.0.2")
-#git_merge_release_to_master_and_tag("2.0.2")
-
-#git_create_rc_branch("2.1RC")
-
-#git_create_branch_for_tag_release("2.1.1")
-#git_merge_release_to_master_and_tag("2.1.1")
-#git_create_branch_for_tag_release("2.1.1")
-#git_merge_release_to_master_and_tag("2.1.1")
 
 
 
